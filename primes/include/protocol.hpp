f6a59b636cdd69c8c89bbfaeac8f129cca27f682
Pavel Begunkov <asml.silence@gmail.com>
2018-04-11 22:33:03 +0300

    net: Add protocol constants
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

---
+++ primes/include/protocol.hpp
@@ -0,0 +1,11 @@
+#ifndef PRIMES_COMMANDS_HPP
+#define PRIMES_COMMANDS_HPP
+
+#define CMD_GET_LAST_N_QUERY        1
+#define CMD_GET_LAST_N_RESP         2
+#define CMD_COMP_RANGE_QUERY        3
+#define CMD_OMP_RANGE_RESP          4
+
+#define PRIME_PORT  13666
+
+#endif //PRIMES_COMMANDS_HPP
