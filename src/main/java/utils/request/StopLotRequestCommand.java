9b7352a9e9b520cb376ed2474ec4ec720c851f5c
Nikolay <n-zhidkov@yandex.ru>
2018-04-22 18:50:43 +0300

    requests commands impl

--- src/main/java/utils/request/StopLotRequestCommand.java
+++ src/main/java/utils/request/StopLotRequestCommand.java
@@ -1,19 +1,45 @@
 package utils.request;
 
 import http.HttpRequest;
+import org.json.JSONObject;
 import utils.API;
 
+import java.net.URI;
+import java.nio.file.Paths;
+
 public class StopLotRequestCommand implements RequestCommand {
+
+    private final static API api = API.STOP_LOT;
+
+    private final int lotId;
+
+    public StopLotRequestCommand(int lotId) {
+        this.lotId = lotId;
+    }
+
+    public int getLotId() {
+        return lotId;
+    }
+
     public StopLotRequestCommand(HttpRequest httpRequest) {
+        final String[] parts = httpRequest.getURI().getPath().split("/");
+        this.lotId = Integer.valueOf(parts[1]);
     }
 
     @Override
     public HttpRequest toHttpRequest() {
-        return null;
+        return new HttpRequest(buildUri(), api.getHttpMethod(), new JSONObject());
+    }
+
+    private URI buildUri() {
+        return URI.create(Paths.get(
+                "/" + api.getUriStart())
+                .resolve(String.valueOf(lotId))
+                .toString());
     }
 
     @Override
     public API getAPI() {
-        return API.STOP_LOT;
+        return api;
     }
 }
