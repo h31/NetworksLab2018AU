463fa7126530dba95dfae022d69f48a99f2acfb8
Plyushchenko <plyushenko@bk.ru>
2018-04-22 20:58:57 +0300

    fix

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -2,13 +2,36 @@ package http;
 
 import org.json.JSONObject;
 
-public class HttpResponse {
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpResponse extends HttpPacket {
+    public final static int STATUS_OK = 200;
+    private final int status;
+    private final JSONObject body;
+
+    public HttpResponse(int status, JSONObject body) {
+        super(buildStartLine(status), buildHeaders(body), body);
+        this.status = status;
+        this.body = body;
+    }
+    
+
+    private static String buildStartLine(int status) {
+        return VERSION + " " + status;
+    }
+
+    private static Map<String,String> buildHeaders(JSONObject body) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(CONTENT_LENGTH, Integer.toString(body.length()));
+        return headers;
+    }
 
     public int getStatus() {
-        return 0;
+        return status;
     }
 
     public JSONObject getBody() {
-        return null;
+        return body;
     }
 }
