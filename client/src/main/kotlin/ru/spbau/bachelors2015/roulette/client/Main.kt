31851d7a9d45e159c2ca44f754c222a9d9ddfbce
Ivan Veselov <veselov.iva@gmail.com>
2018-04-25 16:36:54 +0300

    Return redundant imports.

--- client/src/main/kotlin/ru/spbau/bachelors2015/roulette/client/Main.kt
+++ client/src/main/kotlin/ru/spbau/bachelors2015/roulette/client/Main.kt
@@ -1,5 +1,7 @@
 package ru.spbau.bachelors2015.roulette.client
 
+import ru.spbau.bachelors2015.roulette.protocol.highlevel.RegistrationRequest
+
 fun main(args: Array<String>) {
 }
 
--- server/src/main/kotlin/ru/spbau/bachelors2015/roulette/server/Main.kt
+++ server/src/main/kotlin/ru/spbau/bachelors2015/roulette/server/Main.kt
@@ -1,4 +1,6 @@
 package ru.spbau.bachelors2015.roulette.server
 
+import ru.spbau.bachelors2015.roulette.protocol.highlevel.RegistrationRequest
+
 fun main(args: Array<String>) {
 }
