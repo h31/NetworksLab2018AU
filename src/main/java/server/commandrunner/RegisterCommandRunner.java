4aa1a0daf5ee6bb16fc20f15d1b16ef2029514d5
Plyushchenko <plyushenko@bk.ru>
2018-04-25 12:40:09 +0300

    registerCommandRunner fix

--- src/main/java/server/commandrunner/RegisterCommandRunner.java
+++ src/main/java/server/commandrunner/RegisterCommandRunner.java
@@ -5,6 +5,7 @@ import utils.API;
 import utils.data.User;
 import utils.request.RegisterRequestCommand;
 import utils.request.RequestCommand;
+import utils.response.ForbiddenResponseCommand;
 import utils.response.RegisterResponseCommand;
 import utils.response.ResponseCommand;
 
@@ -20,6 +21,9 @@ public class RegisterCommandRunner implements CommandRunner {
         RegisterRequestCommand registerRequestCommand = (RegisterRequestCommand) requestCommand;
         User user = registerRequestCommand.getUser();
         String executionResult = NO_ERROR;
+        if (user != null) {
+            return new ForbiddenResponseCommand("User at this connection already registered");
+        }
         if (context.containsUser(user)) {
             executionResult = "User already registered: " + user.getName() + " " + user.getRole();
         } else {
