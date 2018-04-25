96cf213eeb0c6e492f3f8c047cd897ebac4b0592
Ivan Veselov <veselov.iva@gmail.com>
2018-04-25 22:23:54 +0300

    Add ClientCommunicationSocket full interface.

--- protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/ClientCommunicationSocket.kt
+++ protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/ClientCommunicationSocket.kt
@@ -4,10 +4,43 @@ import ru.spbau.bachelors2015.roulette.protocol.http.HttpInputStream
 import ru.spbau.bachelors2015.roulette.protocol.http.HttpOutputStream
 import ru.spbau.bachelors2015.roulette.protocol.http.HttpResponseStatus
 import java.io.Closeable
+import java.net.ProtocolException
 import java.net.Socket
 
 interface RegistrationResponseHandler {
     fun handle(response: OkResponse)
+
+    fun handle(response: ErrorResponse)
+}
+
+interface GameStartResponseHandler {
+    fun handle(response: GameStartResponse)
+
+    fun handle(response: ErrorResponse)
+}
+
+interface GameStatusResponseHandler {
+    fun handle(response: GameStatusPositiveResponse)
+
+    fun handle(response: GameStatusNegativeResponse)
+
+    fun handle(response: ErrorResponse)
+}
+
+interface BalanceResponseHandler {
+    fun handle(response: BalanceResponse)
+
+    fun handle(response: ErrorResponse)
+}
+
+interface BetResponseHandler {
+    fun handle(response: OkResponse)
+
+    fun handle(response: ErrorResponse)
+}
+
+interface GameResultsResponseHandler {
+    fun handle(response: GameResultsResponse)
 
     fun handle(response: ErrorResponse)
 }
@@ -20,8 +53,8 @@ class ClientCommunicationSocket(private val underlyingSocket: Socket): Closeable
 
     private val outputStream = HttpOutputStream(underlyingSocket.getOutputStream())
 
-    fun send(registrationRequest: RegistrationRequest, handler: RegistrationResponseHandler) {
-        outputStream.write(registrationRequest.toHttpRepresentation())
+    fun send(request: RegistrationRequest, handler: RegistrationResponseHandler) {
+        outputStream.write(request.toHttpRepresentation())
         val response = inputStream.readHttpResponse()
 
         when (response.status) {
@@ -30,7 +63,29 @@ class ClientCommunicationSocket(private val underlyingSocket: Socket): Closeable
             HttpResponseStatus.BAD_REQUEST -> handler.handle(
                 ErrorResponse(response.messageBody ?: "")
             )
+
+            HttpResponseStatus.NO_CONTENT -> throw ProtocolException("Unexpected response status")
         }
+    }
+
+    fun send(request: GameStartRequest, handler: GameStartResponseHandler) {
+        TODO("Implement")
+    }
+
+    fun send(request: GameStatusRequest, handler: GameStatusResponseHandler) {
+        TODO("Implement")
+    }
+
+    fun send(request: BalanceRequest, handler: BalanceResponseHandler) {
+        TODO("Implement")
+    }
+
+    fun send(request: BetRequest, handler: BetResponseHandler) {
+        TODO("Implement")
+    }
+
+    fun send(request: GameResultsRequest, handler: GameResultsResponseHandler) {
+        TODO("Implement")
     }
 
     /**
