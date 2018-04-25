d6e990e260400dae55f5eee8488d4c9774b43fb4
Ivan Veselov <veselov.iva@gmail.com>
2018-04-25 18:00:12 +0300

    Add all requests classes.

--- protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/Requests.kt
+++ protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/Requests.kt
@@ -52,3 +52,92 @@ class RegistrationRequest(
         private val nicknameQueryLineKey = "nickname"
     }
 }
+
+class GameStartRequest: Request() {
+    override val resourcePath = GameStartRequest.resourcePath
+
+    override val requestMethod = GameStartRequest.requestMethod
+
+    override fun queryLine(): QueryLine {
+        return QueryLine()
+    }
+
+    companion object {
+        val resourcePath: ResourcePath = ResourcePath("start-game")
+
+        val requestMethod: HttpRequestMethod = HttpRequestMethod.PUT
+    }
+}
+
+class GameStatusRequest: Request() {
+    override val resourcePath = GameStatusRequest.resourcePath
+
+    override val requestMethod = GameStatusRequest.requestMethod
+
+    override fun queryLine(): QueryLine {
+        return QueryLine()
+    }
+
+    companion object {
+        val resourcePath: ResourcePath = ResourcePath("game-status")
+
+        val requestMethod: HttpRequestMethod = HttpRequestMethod.GET
+    }
+}
+
+class BalanceRequest: Request() {
+    override val resourcePath = BalanceRequest.resourcePath
+
+    override val requestMethod = BalanceRequest.requestMethod
+
+    override fun queryLine(): QueryLine {
+        return QueryLine()
+    }
+
+    companion object {
+        val resourcePath: ResourcePath = ResourcePath("balance")
+
+        val requestMethod: HttpRequestMethod = HttpRequestMethod.GET
+    }
+}
+
+class BetRequest(val gameId: Int, val value: Int): Request() {
+    override val resourcePath = BetRequest.resourcePath
+
+    override val requestMethod = BetRequest.requestMethod
+
+    override fun queryLine(): QueryLine {
+        return QueryLine(
+            Pair(gameIdQueryLineKey, gameId.toString()),
+            Pair(valueQueryLineKey, value.toString())
+        )
+    }
+
+    companion object {
+        val resourcePath: ResourcePath = ResourcePath("bet")
+
+        val requestMethod: HttpRequestMethod = HttpRequestMethod.PUT
+
+        private val gameIdQueryLineKey = "id"
+
+        private val valueQueryLineKey = "value"
+    }
+}
+
+class GameResultsRequest(val gameId: Int): Request() {
+    override val resourcePath = GameResultsRequest.resourcePath
+
+    override val requestMethod = GameResultsRequest.requestMethod
+
+    override fun queryLine(): QueryLine {
+        return QueryLine(Pair(gameIdQueryLineKey, gameId.toString()))
+    }
+
+    companion object {
+        val resourcePath: ResourcePath = ResourcePath("game-results")
+
+        val requestMethod: HttpRequestMethod = HttpRequestMethod.GET
+
+        private val gameIdQueryLineKey = "id"
+    }
+}
