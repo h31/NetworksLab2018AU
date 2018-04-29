5f22b9ee5fc13f2889ea65ed4b4e48534a39d058
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-04-29 20:06:14 +0300

    more powerful hello world

--- online_shop/protocol/src/main/java/Main.java
+++ online_shop/protocol/src/main/java/Main.java
@@ -1,12 +1,35 @@
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url = "google.co.uk";
-        try (Socket socket = new Socket(url, 80)) {
+        Thread server = new Thread(() -> {
+            try (
+                    ServerSocket serverSocket = new ServerSocket(11111);
+                    Socket socket = serverSocket.accept();
+            ) {
+
+                HttpRequest request = HttpRequest.parse(socket.getInputStream());
+                String requestURL = request.getUrl();
+                List<String> responseBody = Arrays.asList(requestURL, "666");
+                HttpResponse response = new HttpResponse(200, "OK", responseBody);
+                response.dump(socket.getOutputStream());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.start();
+
+
+        String url = "localhost";
+        try (Socket socket = new Socket(url, 11111)) {
             socket.setSoTimeout(1000);
-            HttpRequest request = new HttpRequest("GET", "/intl/en/policies/privacy/");
+            HttpRequest request = new HttpRequest("GET", "/superduperitemprice");
             System.out.println(request);
             request.dump(socket.getOutputStream());
             HttpResponse response = HttpResponse.parse(socket.getInputStream());
