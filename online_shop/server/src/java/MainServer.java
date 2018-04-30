187793d28df9f5947ec6a604576e6c961b50c5ca
ekaterinaMaljutina <e.maljutina@gmail.com>
2018-04-30 16:17:11 +0300

    server up

--- online_shop/server/src/java/MainServer.java
+++ online_shop/server/src/java/MainServer.java
@@ -3,28 +3,76 @@ package src.java;
 import src.main.java.HttpRequest;
 import src.main.java.HttpResponse;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainServer {
 
-    public static void main(String[] args) {
+    private Map<String, Socket> clients = new HashMap<>();
+
+
+    class ClientThread implements Runnable {
+
+        private String nameClient;
+        private boolean isActive = true;
+
+        public ClientThread(String nameClient) {
+            this.nameClient = nameClient;
+        }
+
+        public String getNameClient() {
+            return nameClient;
+        }
+
+        public boolean isActive() {
+            return isActive;
+        }
+
+        @Override
+        public void run() {
+            Socket clientSocket = clients.get(nameClient);
+            while (isActive && !clientSocket.isClosed()) {
+                try {
+                    HttpRequest request = HttpRequest.parse(clientSocket.getInputStream());
+                    String requestURL = request.getUrl();
+                    List<String> responseBody = Arrays.asList(requestURL, " HelloBoris");
+                    HttpResponse response = new HttpResponse(200, "OK",
+                            responseBody);
+                    response.dump(clientSocket.getOutputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void runServer() {
         while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(
-                    Utils.PORT_SERVER);
-            ) {
+            try (ServerSocket serverSocket = new ServerSocket(Utils.PORT_SERVER)) {
                 Socket clientSocket = serverSocket.accept();
-                HttpRequest request = HttpRequest.parse(clientSocket.getInputStream());
-                String requestURL = request.getUrl();
-                List<String> responseBody = Arrays.asList(requestURL, " HelloBoris");
-                HttpResponse response = new HttpResponse(200, "OK", responseBody);
-                response.dump(clientSocket.getOutputStream());
+
+                ClientThread client = new ClientThread("name");
+                clients.put(client.getNameClient(), clientSocket);
+                new Thread(client).start();
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+    }
+
+
+    public static void main(String[] args) {
+        MainServer server = new MainServer();
+        Thread main_server = new Thread(server::runServer);
+        main_server.start();
+
     }
 }
