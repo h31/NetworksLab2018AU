b65729ff4d9cabcd12339cbd830ee67b85a7b9e7
Ivan Veselov <veselov.iva@gmail.com>
2018-05-05 00:17:36 +0300

    Add socket closing handling to ServerCommunicationSocket.

--- protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/ServerCommunicationSocket.kt
+++ protocol/src/main/kotlin/ru/spbau/bachelors2015/roulette/protocol/highlevel/ServerCommunicationSocket.kt
@@ -3,6 +3,7 @@ package ru.spbau.bachelors2015.roulette.protocol.highlevel
 import ru.spbau.bachelors2015.roulette.protocol.http.HttpInputStream
 import ru.spbau.bachelors2015.roulette.protocol.http.HttpOutputStream
 import java.io.Closeable
+import java.io.IOException
 import java.net.Socket
 
 interface RequestHandler {
@@ -21,42 +22,45 @@ interface RequestHandler {
     fun handleSocketClosing()
 }
 
-// TODO: need to handle socket closing
 class ServerCommunicationSocket(private val underlyingSocket: Socket): Closeable {
     private val inputStream = HttpInputStream(underlyingSocket.getInputStream())
 
     private val outputStream = HttpOutputStream(underlyingSocket.getOutputStream())
 
     fun handleNextRequest(handler: RequestHandler) {
-        val request = inputStream.readHttpRequest()
+        try {
+            val request = inputStream.readHttpRequest()
 
-        val response = try {
-            when (request.uri.path) {
-                RegistrationRequest.resourcePath ->
-                    handler.handle(RegistrationRequest.fromHttpRepresentation(request))
+            val response = try {
+                when (request.uri.path) {
+                    RegistrationRequest.resourcePath ->
+                        handler.handle(RegistrationRequest.fromHttpRepresentation(request))
 
-                GameStartRequest.resourcePath ->
-                    handler.handle(GameStartRequest.fromHttpRepresentation(request))
+                    GameStartRequest.resourcePath ->
+                        handler.handle(GameStartRequest.fromHttpRepresentation(request))
 
-                GameStatusRequest.resourcePath ->
-                    handler.handle(GameStatusRequest.fromHttpRepresentation(request))
+                    GameStatusRequest.resourcePath ->
+                        handler.handle(GameStatusRequest.fromHttpRepresentation(request))
 
-                BalanceRequest.resourcePath ->
-                    handler.handle(BalanceRequest.fromHttpRepresentation(request))
+                    BalanceRequest.resourcePath ->
+                        handler.handle(BalanceRequest.fromHttpRepresentation(request))
 
-                BetRequest.resourcePath ->
-                    handler.handle(BetRequest.fromHttpRepresentation(request))
+                    BetRequest.resourcePath ->
+                        handler.handle(BetRequest.fromHttpRepresentation(request))
 
-                GameResultsRequest.resourcePath ->
-                    handler.handle(GameResultsRequest.fromHttpRepresentation(request))
+                    GameResultsRequest.resourcePath ->
+                        handler.handle(GameResultsRequest.fromHttpRepresentation(request))
 
-                else -> ErrorResponse("Unknown resource")
+                    else -> ErrorResponse("Unknown resource")
+                }
+            } catch (_: InvalidHttpRequest) {
+                ErrorResponse("Invalid request format")
             }
-        } catch (_: InvalidHttpRequest) {
-            ErrorResponse("Invalid request format")
-        }
 
-        outputStream.write(response.toHttpRepresentation())
+            outputStream.write(response.toHttpRepresentation())
+        } catch (_: IOException) {
+            handler.handleSocketClosing()
+        }
     }
 
     /**
